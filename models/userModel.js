3ea9aad15884095c4618f6d89ecbe309f8c6a86e
DKKowalski <52242924+DKKowalski@users.noreply.github.com>
2024-03-07 10:55:37 +0000

    Update userModel.js
    Made changes to the userModel

--- models/userModel.js
+++ models/userModel.js
@@ -1,18 +1,11 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  name: {
+  email: {
     type: String,
     required: [true, "A user must have a name"],
     trim: true,
     unoque: true,
-  },
-
-  username: {
-    type: String,
-    required: [true, `A user must have a username`],
-    unique: true,
-    trim: true,
   },
 
   password: {
